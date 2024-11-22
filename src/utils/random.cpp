8f71dbbf9fd8dd58db923435a948ceaf0863462d
Liiizak <144727441+Liiizak@users.noreply.github.com>
2024-11-22 12:00:54 +0300

    changed utils/random.cpp

--- src/utils/random.cpp
+++ src/utils/random.cpp
@@ -1,17 +1,13 @@
-#include <random>
-
-#include <iostream>
 #include "random.hpp"
+
+#include <userver/utils/rand.hpp>
 
 namespace real_medium::utils::random {
 std::string GenerateSalt() {
-  std::random_device dev;
-  std::mt19937 rng(dev());
-  std::uniform_int_distribution<std::mt19937::result_type> dist(48,122);
   const int kSaltLength = 32;
   std::string salt;
   for (size_t i = 0; i < kSaltLength; ++i) {
-    salt += static_cast<char>(dist(rng));
+    salt += userver::utils::RandRange(32, 126);
   }
   return salt;
 }

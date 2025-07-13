e9165a1433ac66c4b0ff69f252538f179a80a13f
Antony Polukhin <antoshkka@gmail.com>
2025-07-13 17:37:13 +0300

    fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,10 +9,7 @@ from testsuite.databases.pgsql import discover
 sys.path.append(os.path.join(os.path.dirname(__file__), 'helpers'))
 
 
-pytest_plugins = [
-    'pytest_userver.plugins.core',
-    'pytest_userver.plugins.postgresql',
-]
+pytest_plugins = ['pytest_userver.plugins.postgresql']
 
 
 @pytest.fixture(scope='session')
